445c791a8dc8c597b592d4526d04049ff22b2d18
thulasiram <test.hkj@gmail.com>
2022-11-28 11:16:08 +0530

    adding app file

---
+++ app.py
@@ -0,0 +1,98 @@
+
+####################simple program################
+#print("Hello world")
+#print("Hey man this is my first program")
+#print("Enjoy the weekend")
+
+##############Variables#######################
+
+#variables are temporary storage location in computer memory
+
+
+#age=34
+#name='thulasi'
+
+#print(name, age)
+
+#first_name="thulasiram"
+#is_online=False
+
+
+##################iput function###################
+
+#name=input("what is your goood name?" )
+
+#print("Welcome" + name)
+
+
+
+################Type Conversion####################
+
+"""
+birth_year = input("Enter your birth year: ")
+
+#by default input function will take string if we enter integer also as dynamic input so we need to convert int
+
+age = 2022-int(birth_year)
+
+name = "thulasiram"
+
+print("your " +name)
+
+#you cont print int along with string
+
+print ("your age is :"  +age)
+
+
+
+"""
+#############builtin functions############
+
+"""
+int()
+float()
+str()
+bool()
+
+"""
+
+"""
+first_number = input("first number: " )
+second_number = input("second number: " )
+
+sum = int(first_number)+int(second_number)
+
+print("sum of the two numbers is :" + str(sum))
+
+"""
+
+first_number = float(input("first number: "))
+second_number = float(input("second number: "))
+
+sum = first_number+second_number
+
+print("sum of the two numbers is :" + str(sum))
+
+
+##############Strings#########################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
